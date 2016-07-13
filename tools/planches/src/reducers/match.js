263d192080cd3cb5046e714cf7ba1e76ae596ab4
Alexandre Girard <git@alexgirard.com>
2016-07-13 16:14:42 +0200

    add score ranking on loadMatchJson

--- tools/planches/src/reducers/match.js
+++ tools/planches/src/reducers/match.js
@@ -43,6 +43,8 @@ function loadMatchJson(state, action) {
 
   json.forEach(item => {
 
+    item.output = item.output.sort((a, b) => a.features.score - b.features.score);
+
     const selectedItem = item.output.some(output => {
       return output.selected;
     });
