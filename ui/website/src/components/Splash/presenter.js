162fe0a4a2ef578a651a3d1a4fbe4f8c11447ef7
Alexandre Girard <git@alexgirard.com>
2016-08-04 17:34:02 +0200

    fix img scale on Splash page #56

--- ui/website/src/components/Splash/presenter.js
+++ ui/website/src/components/Splash/presenter.js
@@ -31,51 +31,66 @@ class Splash extends React.Component {
 
       const selectedOutput = match.output.filter(item => item.selected)[0];
 
-      return (<div>
+      return (<div className="splashComponent">
 
-        <div className="container" style={[styles.fullHeight]}>
-          <div className="row">
-            <div className="col-md-6">
-
-              <h2 style={styles.h2}>Searching for match</h2>
-
-              <ul style={styles.ul}>
-              {
-                selectedOutput.features.in.visual_similarity ?
-                  <li style={styles.ul.li}>Visual similarity: {selectedOutput.features.in.visual_similarity.score}%</li> : ''
-              }
-              {
-                selectedOutput.features.in.metadata_crossover ?
-                  <li style={styles.ul.li}>Metadata crossover: {selectedOutput.features.in.metadata_crossover.score}%</li> : ''
-              }
-              {
-                selectedOutput.features.in.emotional_likeness ?
-                  <li style={styles.ul.li}>Emotional likeness: {selectedOutput.features.in.emotional_likeness.score}%</li> : ''
-              }
-              </ul>
-
-              <p style={styles.share}>Share</p>
-
-              <img className="img-responsive" src={match.input.img} />
-
-              <p style={styles.imgDescription}>
-                {match.input.meta.date}<br/>
-                {match.input.meta.caption}<br/>
-                {match.input.meta.origin}
-              </p>
-
-            </div>
-            <div className="col-md-6 nopadding">
+        <div className="container splashContainer" style={[styles.fullHeight]}>
+          <div className="row" style={styles.fullHeight.row}>
+            <div className="col-md-6" style={styles.fullHeight.col}>
 
               <div className="row">
-                <div className="col-md-10 col-md-offset-1">
-                  <img className="img-responsive" style={styles.img} src={selectedOutput.img} />
+                <div className="col-md-12">
+
+                  <h2 style={styles.h2}>Searching for match</h2>
+
+                  <ul style={styles.ul}>
+                  {
+                    selectedOutput.features.in.visual_similarity ?
+                      <li style={styles.ul.li}>Visual similarity: {selectedOutput.features.in.visual_similarity.score}%</li> : ''
+                  }
+                  {
+                    selectedOutput.features.in.metadata_crossover ?
+                      <li style={styles.ul.li}>Metadata crossover: {selectedOutput.features.in.metadata_crossover.score}%</li> : ''
+                  }
+                  {
+                    selectedOutput.features.in.emotional_likeness ?
+                      <li style={styles.ul.li}>Emotional likeness: {selectedOutput.features.in.emotional_likeness.score}%</li> : ''
+                  }
+                  </ul>
+
+                  <p style={styles.share}>Share</p>
+
+                </div>
+              </div>
+
+              <div className="row">
+                <div className="col-md-12">
+
+                  <img src={match.input.img} style={[styles.fullHeight.img]}/>
+
+                  <p style={styles.imgDescription}>
+                    {match.input.meta.date}<br/>
+                    {match.input.meta.caption}<br/>
+                    {match.input.meta.origin}
+                  </p>
+
+                </div>
+              </div>
+
+            </div>
+
+            <div className="col-md-6 nopadding">
+
+              <div className="row"  style={styles.fullHeight.row}>
+                <div className="col-md-10 col-md-offset-1" style={styles.fullHeight.col}>
+
+                  <img style={[styles.fullHeight.img]} src={selectedOutput.img} />
 
                   <p style={styles.imgDescription}>
                     {selectedOutput.meta.date}<br/>
                     {selectedOutput.meta.title}<br/>
                     {selectedOutput.meta.origin}
                   </p>
+
                 </div>
               </div>
 
