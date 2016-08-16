2d39267afacda5898d821f0a40f3d908aaa5c8d0
Emmanuel Benazera <beniz@droidnik.fr>
2016-08-16 14:07:25 +0200

    added summary to each match, ref#78

--- ai/src/ensembling.py
+++ ai/src/ensembling.py
@@ -23,28 +23,41 @@ under the License.
 
 class EnsemblingScores:
     def __init__(self):
-        self.factors = {'composition':0.3,'places_composition':0.3,'places':0.2,'categories':0.1,'densecap':0.5,'mapi_tags':0.1,'mapi_cats':0.1,'mapi':0.05,'txtembed':0.1}
+        self.factors = {'composition':0.3,'places_composition':0.3,'places':0.2,'categories':0.1,'densecap':0.5,'mapi_tags':0.1,'mapi_cats':0.1,'mapi':0.05,'txtembed':0.3}
+        self.summary_map = {'composition':'composition','places_composition':'composition','places':'context','categories':'context','densecap':'objects','mapi_cats':'context','mapi_tags':'context','mapi':'faces','txtembed':'context'}
         return
 
     # json_out in UI format, simple additive ensembling
     def ensembling(self,json_out):
         for k,m in json_out.iteritems(): # iterate matches
             for o in m['output']: # iterate candidates for a given match
+                summary = {'scores':{'objects':0.0,'faces':0.0,'composition':0.0,'context':0.0},
+                           'processing_time':0.0}
+                summary_sum = 0.0
                 final_score = 0.0
                 comp_score = 0.0 # compositional score
                 comp_num = 0.0
                 for g,v in o['features']['out'].iteritems(): # iterate generators of a match
-                    factor = 1.0 #TODO: scoring before selection
+                    factor = 1.0
                     for fa,fv in self.factors.iteritems():
                         if fa in g:
                             factor = fv
                             break
                     if 'composition' in g:
                         comp_num += 1.0
-                        comp_score += (1.0/comp_num) * factor*v['score']
+                        comp_score += factor*v['score']#(1.0/comp_num) * factor*v['score']
+                        summary['scores'][self.summary_map['composition']] += 1.0
+                    elif 'categories' in g:
+                        summary['scores'][self.summary_map['categories']] += 1.0
                     else:
                         final_score += factor*v['score']
+                        summary['scores'][self.summary_map[g]] += 1.0
+                    summary_sum += 1.0
                 if comp_num > 0.0:
-                    final_score += comp_score# / comp_num
+                    final_score += comp_score / comp_num
                 o['features']['score'] = final_score
+                for s,sv in summary['scores'].iteritems():
+                    sv /= summary_sum
+                    summary['scores'][s] = sv
+                o['features']['summary'] = summary
         return json_out
