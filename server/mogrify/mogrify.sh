86381c0b0cee1e989c261988f1a88f940723061f
Alexandre Girard <git@alexgirard.com>
2016-08-17 14:43:41 +0200

    use parallel command for mogrify script

--- server/mogrify/mogrify.sh
+++ server/mogrify/mogrify.sh
@@ -9,18 +9,7 @@ mkdir -p responsive_480
 mkdir -p responsive_757
 mkdir -p responsive_1920
 
-for i in *_3_*.JPG; do
-   echo ${i}
-  if [ ! -f "${picture_root}responsive_1920/${i}" ]; then
-    mogrify -path "${picture_root}responsive_1920/" -filter Triangle -define filter:support=2 -thumbnail 1920 -unsharp 0.25x0.08+8.3+0.045 -dither None -posterize 136 -quality 82 -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace none -colorspace sRGB "$i"
-  fi
-  if [ ! -f "${picture_root}responsive_757/${i}" ]; then
-    mogrify -path "${picture_root}responsive_757/" -filter Triangle -define filter:support=2 -thumbnail 757 -unsharp 0.25x0.08+8.3+0.045 -dither None -posterize 136 -quality 82 -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace none -colorspace sRGB "${picture_root}responsive_1920/${i}"
-  fi
-  if [ ! -f "${picture_root}responsive_480/${i}" ]; then
-    mogrify -path "${picture_root}responsive_480/" -filter Triangle -define filter:support=2 -thumbnail 480 -unsharp 0.25x0.08+8.3+0.045 -dither None -posterize 136 -quality 82 -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace none -colorspace sRGB "${picture_root}responsive_757/${i}"
-  fi
-  if [ ! -f "${picture_root}responsive_375/${i}" ]; then
-    mogrify -path "${picture_root}responsive_375/" -filter Triangle -define filter:support=2 -thumbnail 375 -unsharp 0.25x0.08+8.3+0.045 -dither None -posterize 136 -quality 82 -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace none -colorspace sRGB "${folder}responsive_480/${i}"
-  fi
-done
+find . -maxdepth 1 -iname "*.jpg" -type f -print0 | parallel --progress -0 -j +0 'if [ ! -f responsive_1920/{/} ]; then mogrify -path responsive_1920/ -filter Triangle -define filter:support=2 -thumbnail 1920 -unsharp 0.25x0.08+8.3+0.045 -dither None -posterize 136 -quality 82 -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace none -colorspace sRGB {}; fi'
+find . -maxdepth 1 -iname "*.jpg" -type f -print0 | parallel --progress -0 -j +0 'if [ ! -f responsive_757/{/} ]; then mogrify -path responsive_757/ -filter Triangle -define filter:support=2 -thumbnail 757 -unsharp 0.25x0.08+8.3+0.045 -dither None -posterize 136 -quality 82 -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace none -colorspace sRGB {}; fi'
+find . -maxdepth 1 -iname "*.jpg" -type f -print0 | parallel --progress -0 -j +0 'if [ ! -f responsive_480/{/} ]; then mogrify -path responsive_480/ -filter Triangle -define filter:support=2 -thumbnail 480 -unsharp 0.25x0.08+8.3+0.045 -dither None -posterize 136 -quality 82 -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace none -colorspace sRGB {}; fi'
+find . -maxdepth 1 -iname "*.jpg" -type f -print0 | parallel --progress -0 -j +0 'if [ ! -f responsive_375/{/} ]; then mogrify -path responsive_375/ -filter Triangle -define filter:support=2 -thumbnail 375 -unsharp 0.25x0.08+8.3+0.045 -dither None -posterize 136 -quality 82 -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace none -colorspace sRGB {}; fi'
