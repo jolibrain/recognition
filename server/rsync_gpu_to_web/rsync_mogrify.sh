e3ee8c93648bdb185a182d6cbae2a4637033821d
Alexandre Girard <git@alexgirard.com>
2016-08-29 13:26:16 +0200

    correct rsync script

--- server/rsync_gpu_to_web/rsync_mogrify.sh
+++ server/rsync_gpu_to_web/rsync_mogrify.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-rsync --protect-args -avz -e ssh recog_gpu:"/data/tate/imgs/reuters/RPA Feed for Tate/*.JPG" /data/tate/imgs/reuters/RPA\ Feed\ for\ Tate/
+rsync --copy-unsafe-links --protect-args -KLavz -e ssh recog_gpu:"/data/tate/imgs/reuters/RPA Feed for Tate/*" /data/tate/imgs/reuters/RPA\ Feed\ for\ Tate/
 #/home/recog/recognition/server/mogrify/mogrify.sh
 /home/recog/local/bin/node /home/recog/recognition/server/og_image/og_image.js
