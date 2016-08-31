ebe965f0101eaf1e9501e13c8b8897325073159b
Emmanuel Benazera <beniz@droidnik.fr>
2016-08-31 15:32:32 +0200

    putting mapi captions on

--- ai/tools/generate_matches.sh
+++ ai/tools/generate_matches.sh
@@ -2,7 +2,8 @@
 DATE=`date +%Y-%m-%d:%H:%M:%S`
 #export $TERM=xterm
 cd /home/recog/tate/dev/recognition/ai/tools/
-python recog_matches.py --input-imgs /data/tate/imgs/reuters/RPA\ Feed\ for\ Tate/ --generators all --indexes-repo /home/recog/tate/data/recog_test/indexes/ --models-repo /home/recog/tate/data/models/ --json-output matches_hourly.json --nfiles 200 --nmatches 10 --last-hour 1 --website --medium --freq-filter smatches.bin --concat
+#python recog_matches.py --input-imgs /data/tate/imgs/reuters/RPA\ Feed\ for\ Tate/ --generators all --indexes-repo /home/recog/tate/data/recog_test/indexes/ --models-repo /home/recog/tate/data/models/ --json-output matches_hourly.json --nfiles 200 --nmatches 10 --last-hour 1 --website --medium --freq-filter smatches.bin --concat
+python recog_matches.py --input-imgs /data/tate/imgs/reuters/Images\ of\ the\ Day\ Tate/ --generators all --indexes-repo /home/recog/tate/data/recog_test/indexes/ --models-repo /home/recog/tate/data/models/ --json-output matches_hourly.json --nfiles 200 --nmatches 10 --last-hour 1 --website --medium --freq-filter smatches.bin --concat
 RETVAL=$?
 if [ $RETVAL -eq 0 ]; then
     cp splash.json '/data/tate/matches/splash.json_'$DATE
