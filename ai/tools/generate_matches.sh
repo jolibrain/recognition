d43e56d8f82328aa8ee8e6af2d341846cb5be4ee
Emmanuel Benazera <beniz@droidnik.fr>
2016-09-01 17:39:17 +0200

    updated hourly generation script

--- ai/tools/generate_matches.sh
+++ ai/tools/generate_matches.sh
@@ -3,7 +3,7 @@ DATE=`date +%Y-%m-%d:%H:%M:%S`
 #export $TERM=xterm
 cd /home/recog/tate/dev/recognition/ai/tools/
 #python recog_matches.py --input-imgs /data/tate/imgs/reuters/RPA\ Feed\ for\ Tate/ --generators all --indexes-repo /home/recog/tate/data/recog_test/indexes/ --models-repo /home/recog/tate/data/models/ --json-output matches_hourly.json --nfiles 200 --nmatches 10 --last-hour 1 --website --medium --freq-filter smatches.bin --concat
-python recog_matches.py --input-imgs /data/tate/imgs/reuters/Images\ of\ the\ Day\ Tate/ --generators all --indexes-repo /home/recog/tate/data/recog_test/indexes/ --models-repo /home/recog/tate/data/models/ --json-output match_hourly.json --nfiles 200 --nmatches 50 --last-hour 1 --website --medium --freq-filter smatches.bin --concat --splash-output splash_hourly.json --gallery-output gallery_hourly.json
+python recog_matches.py --input-imgs /data/tate/imgs/reuters/Images\ of\ the\ Day\ Tate/ --generators all --indexes-repo /home/recog/tate/data/recog_test/indexes/ --models-repo /home/recog/tate/data/models/ --json-output match_hourly.json --nfiles 200 --nmatches 50 --last-hour 1 --website --medium --freq-filter smatches.bin --concat --splash-output splash_hourly.json --gallery-output gallery_hourly.json --event-filter
 RETVAL=$?
 if [ $RETVAL -eq 0 ]; then
     cp splash_hourly.json '/data/tate/matches/splash.json_'$DATE
