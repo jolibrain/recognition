793b0430c80e2cdfcd32b1b18a8638d740af05f3
Alexandre Girard <git@alexgirard.com>
2016-11-28 17:19:06 +0100

    change placeholder style - #246

--- ui/website/src/components/Placeholder/index.js
+++ ui/website/src/components/Placeholder/index.js
@@ -21,7 +21,16 @@ import styles from './styles.js';
 class Placeholder extends React.Component {
   render() {
     return <div style={styles.uppercase} className="infoPage">
-      <div className="fluid-container" style={{margin: "0 auto", width: "50%"}}>
+      <div className="hidden-xs fluid-container" style={{margin: "0 auto", width: "50%"}}>
+        <div className="row">
+          <div className="col-xs-12">
+            <p className="font-subtext infoDescription" style={{color: "#FFFFFF"}}>RECOGNITION HAS NOW BEEN TURNED OFF.</p>
+              <p className="font-subtext infoDescription" style={{color: "#FFFFFF"}}>RECOGNITION WAS AN ARTIFICIAL INTELLIGENCE PROGRAM THAT COMPARED UP-TO-THE-MINUTE PHOTOJOURNALISM WITH BRITISH ART FROM THE TATE COLLECTION BETWEEN 2 SEPTEMBER AND 27 NOVEMBER 2016.</p>
+              <p className="font-subtext infoDescription" style={{color: "#FFFFFF"}}>THIS WEBSITE WILL REMAIN AS AN ARCHIVE OF THE MACHINE'S FINDINGS. PLEASE RETURN SOON TO FIND MORE RSULTS FROM THIS EXPERIMENT TO SEE WHETHER MACHINE INTELLIGENCE CAN UNEARTH SIMILARITIES BETWEEN THE ART OF THE PAST AND REPRESENTATIONS OF TODAY'S WORLD.</p>
+          </div>
+        </div>
+      </div>
+      <div className="visible-xs fluid-container">
         <div className="row">
           <div className="col-xs-12">
             <p className="font-subtext infoDescription" style={{color: "#FFFFFF"}}>RECOGNITION HAS NOW BEEN TURNED OFF.</p>
